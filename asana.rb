405280524792cbd490a3756eb6ebccca84149d4b
Jeff Beck <becked@fastmail.fm>
2015-10-19 22:41:23 -0400

    Display hours worked on completed tasks by project and by assignee. More or less done, but will take another look tomorrow. Some folks are not using whole numbers of hours, so maybe deal with that.

--- asana.rb
+++ asana.rb
@@ -18,6 +18,9 @@ class Asana
         request = Net::HTTP::Get.new(uri)
       end
       request.basic_auth(@api_key, "")
+      response = http.request(request)
+      errors = JSON.parse(http.request(request).body)["errors"]
+      puts errors.inspect if errors
       JSON.parse(http.request(request).body)["data"]
     end 
   end
@@ -28,8 +31,37 @@ class Asana
     request(uri)
   end
 
+  def user(user_id)
+    uri = "https://app.asana.com/api/1.0/users/#{user_id}"
+    request(uri)
+  end
+
+  def task(task_id)
+    uri = "https://app.asana.com/api/1.0/tasks/#{task_id}"
+    request(uri)
+  end
+
+  def users_in_workspace(workspace_id)
+    uri = "https://app.asana.com/api/1.0/workspaces/#{workspace_id}/users"
+    request(uri)
+  end
+
+  def tasks_for_assignee_and_workspace(assignee, workspace_id)
+    uri = "https://app.asana.com/api/1.0/tasks/?assignee=#{assignee}&workspace=#{workspace_id}"
+    request(uri)
+  end
+
+  # TODO fix the timezone here
+  # Do we really want to hard set one month to be equal to "recent"?
+  def recent_tasks_for_assignee_and_workspace(assignee, workspace_id)
+    uri = "https://app.asana.com/api/1.0/tasks/?assignee=#{assignee}&workspace=#{workspace_id}&modified_since='#{Time.now.strftime("%Y-%m-01T%H:%M:%S.158Z")}'"
+    request(uri)
+  end
+
   def tasks_for_project(project_id)
-    uri = "https://app.asana.com/api/1.0/projects/#{project_id}/tasks"
+    # Why are there two ways to do this? Which is better? Does it matter?
+    #uri = "https://app.asana.com/api/1.0/projects/#{project_id}/tasks"
+    uri = "https://app.asana.com/api/1.0/tasks/?project=#{project_id}"
     request(uri)
   end
 
--- tasks-get_hours.rb
+++ tasks-get_hours.rb
@@ -1,22 +1,44 @@
 #!/usr/bin/env ruby
+#
+# Display how much time we spend doing project tasks (according to Asana).
+#
+# We preface the task name with the hours it took to complete.
+#   ex. [2] Write script to display hours worked on project tasks in Asana.
 
 require "yaml"
 require_relative "asana.rb"
 
-# Read local config
+# Read local config (assignees, api_key, workspace_id)
 config = YAML.load(File.read("config.yaml"))
 
-# Create a new Asana connection
+# Instantiate Asana
 asana = Asana.new(config["api_key"])
 
-# Get a list of projects
-projects = asana.projects_in_workspace(config["workspace_id"])
-
-# Get tasks for each project
-projects.each do |project|
-  tasks = asana.tasks_for_project(project["id"])
-  puts tasks.first.inspect
+# Grab recently completed tasks from people we care about
+projects = Hash.new(0)
+assignees = Hash.new(0)
+config["assignees"].each do |assignee|
+  asana.recent_tasks_for_assignee_and_workspace(assignee, config["workspace_id"]).
+  map { |e| asana.task(e["id"]) }.
+  # We only care about tasks prefaced with [hours worked]
+  select { |e| e["completed"] == true and e["name"] =~ /^\[\d+\]/ }.each do |task|
+    # Presumably the filter above will make sure this match is always successful ...
+    hours = task["name"].match(/^\[(\d+)\]/)[1].to_i
+    # Not thrilled about having memberships to multiple projects (isn't that we have tagging?)
+    task["projects"].each { |e| projects[e["name"]] += hours }
+    assignees[task["assignee"]["name"]] += hours
+  end
 end
 
+puts
+puts "PROJECTS"
+projects.each do |k,v|
+  puts k + " => " + v.to_s
+end
+puts
 
-
+puts "ASSIGNEES"
+assignees.each do |k,v|
+  puts k + " => " + v.to_s
+end
+puts
